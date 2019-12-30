a6142cc1c9e4bea0f6aaf5c3ce3b62bc509a310f
Ivelin Ivanov <ivelin.atanasoff.ivanov@gmail.com>
2019-12-29 19:53:43 -0600

    fix: progressing with python port

--- src/api.py
+++ src/api.py
@@ -1,96 +1,60 @@
 """Client side abstraction for commonly used REST APIs."""
 
 from util import util
+import time
+import math
 import logging
+import aiohttp
 
 log = logging.getLogger(__name__)
 
+
+async def _fetch(url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as response:
+            return await response
+
+
 class API:
+    """Client side methods for commonly used REST APIs."""
 
-  def __init__(_options):
-      """Create API instance."""
+    def __init__(_options):
+        """Create API instance."""
 
-  def _buildUrl(method: str = None) -> str:
-    const protocol = this._options.secure ? "https://" : "http://";
-    let url =
-      protocol +
-      this._options.host +
-      ":" +
-      this._options.port +
-      this._options.path +
-      this._options.key +
-      "/" +
-      method;
-    const queryString = "?ts=" + new Date().getTime() + "" + Math.random();
-    url += queryString;
+    def _buildUrl(self, method: str = None) -> str:
+        protocol = "https://" if self._options.secure else "http://"
+        url = \
+            protocol + \
+            self._options.host + \
+            ":" + \
+            self._options.port + \
+            self._options.path + \
+            self._options.key + \
+            "/" + \
+            method
+        queryString = "?ts=" + time.monotonous() + "" + math.random()
+        url += queryString
+        return url
 
-    return url;
-  }
-
-  /** Get a unique ID from the server via XHR and initialize with it. */
-  async retrieveId(): Promise<string> {
-    const url = this._buildUrl("id");
-
-    try {
-      const response = await fetch(url);
-
-      if (response.status !== 200) {
-        throw new Error(`Error. Status:${response.status}`);
-      }
-
-      return response.text();
-    } catch (error) {
-      logger.error("Error retrieving ID", error);
-
-      let pathError = "";
-
-      if (
-        this._options.path === "/" &&
-        this._options.host !== util.CLOUD_HOST
-      ) {
-        pathError =
-          " If you passed in a `path` to your self-hosted PeerServer, " +
-          "you'll also need to pass in that same path when creating a new " +
-          "Peer.";
-      }
-
-      throw new Error("Could not get an ID from the server." + pathError);
-    }
-  }
-
-  /** @deprecated */
-  async listAllPeers(): Promise<any[]> {
-    const url = this._buildUrl("peers");
-
-    try {
-      const response = await fetch(url);
-
-      if (response.status !== 200) {
-        if (response.status === 401) {
-          let helpfulError = "";
-
-          if (this._options.host === util.CLOUD_HOST) {
-            helpfulError =
-              "It looks like you're using the cloud server. You can email " +
-              "team@peerjs.com to enable peer listing for your API key.";
-          } else {
-            helpfulError =
-              "You need to enable `allow_discovery` on your self-hosted " +
-              "PeerServer to use this feature.";
-          }
-
-          throw new Error("It doesn't look like you have permission to list peers IDs. " +
-            helpfulError);
-        }
-
-        throw new Error(`Error. Status:${response.status}`);
-      }
-
-      return response.json();
-    } catch (error) {
-      logger.error("Error retrieving list peers", error);
-
-      throw new Error("Could not get list peers from the server." + error);
-    }
-  }
-}
+    async def retrieveId(self):
+        """Get a unique ID from the server and initialize with it."""
+        url = self._buildUrl("id")
+        try:
+            response = await _fetch(url)
+            if response.status != 200:
+                raise ConnectionError(f'Error. Status:{response.status}')
+            return response.text()
+        except Exception as error:
+            log.error("Error retrieving ID: {}", error)
+            pathError = ""
+            if self._options.path == "/" and \
+               self._options.host != util.CLOUD_HOST:
+                pathError = \
+                    " If you passed in a 'path' to your "
+                " self-hosted PeerServer, "
+                " you'll also need to pass in that "
+                " same path when creating a new "
+                " Peer."
+                raise ConnectionError(
+                    "Could not get an ID from the server." +
+                    pathError)
